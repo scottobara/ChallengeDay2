28edf76c72c4aff62378dc388b1c35cc45404265
Scott Obara <scott@rubbscreations.com>
2021-01-09 16:31:30 +1100

    Initial Commit

---
+++ ChallengeDay2/ChallengeDay2App.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeDay2App.swift
+//  ChallengeDay2
+//
+//  Created by Scott Obara on 9/1/21.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeDay2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
